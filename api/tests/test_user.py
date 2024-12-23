62c15183b99cc58f7a8878f90bf71c9a5849f318
afsds <142832572+afsds@users.noreply.github.com>
2024-12-23 15:06:31 -0300

    Update test_user.py

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -6,21 +6,22 @@ from api.models import User
 @pytest.mark.django_db
 def test_create_user():
     user = User.objects.create(
-        email="dark.killa@bol.com.br",
-        username="d4rk_k1ll4",
-        user_pass="ilovekittens@777",
-        first_name="Oscar",
-        last_name="Alho",
+        email="dark.melocoto@bol.com.br",
+        username="d4rk_mecoto",
+        user_pass="iloveliverpo@777",
+        first_name="Paulin",
+        last_name="Pinho",
         country="Brasil",
-        state="São Paulo",
-        city="Piraporinha do Leste",
+        state="Bahia",
+        city="Xique-Xique",
         postal_code="12345-789",
-        address="R. dos Bobos, nº0"
+        address="R. dos Otarios, nº00"
     )
 
-    assert user.username == "d4rk_k1ll4"
-    assert user.first_name == "Oscar"
+    assert user.username == "d4rk_mecoto"
+    assert user.first_name == "Paulin"
     assert user.country == "Brasil"
-    assert user.city == "Piraporinha do Leste"
+    assert user.city == "Xique-Xique"
     assert user.id is not None
+
 
