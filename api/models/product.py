cac9f70c4a56f242d1cb87ff7fe44c4de9102525
afsds <142832572+afsds@users.noreply.github.com>
2024-12-23 15:01:40 -0300

    Update product.py

--- api/models/product.py
+++ api/models/product.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=300, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
+
